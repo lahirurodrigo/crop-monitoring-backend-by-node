fb1d3edb6467eb720ae053d464479f7697ac9866
lahirurodrigo <kmlahirurodrigo@gmail.com>
2025-02-28 01:10:23 +0530

    refactor cropRoute in express app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import cropRoutes from "./routes/cropRoutes";
+import fieldRoutes from "./routes/fieldRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/crops", cropRoutes);
-app.use("/api/fields", cropRoutes);
+app.use("/api/fields", fieldRoutes);
 
 export default app;
